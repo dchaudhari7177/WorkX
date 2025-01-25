f31d278a6d79e8163a1b0cd8b13fcbf6c148ca51
Dipak <dipakchaudhari171@gmail.com>
2025-01-25 19:16:14 +0530

    Navbar added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-indigo-600 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">
+          <Link to="/">WorkX</Link>
+        </h1>
+        <ul className="flex space-x-6">
+          <li>
+            <Link to="/" className="hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" className="hover:underline">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
